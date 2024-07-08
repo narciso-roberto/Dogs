be704476d78153958e5ca5450bb135f00a0e04ac
Narciso Roberto de Souza <narcisoroberto1369@alu.ufc.br>
2024-07-08 10:28:28 -0300

    projeto dogs

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Components/Home'
+import Login from './Components/Login/Login'
+import './app.css'
+
+
+function App() {
+  
+  return (
+    <>
+    <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
